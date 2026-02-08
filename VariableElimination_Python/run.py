b38c9b04c86c254d3ea0deeb92e2400fa5468208
semdi <sem.devries@ru.nl>
2026-02-08 17:57:55 +0100

    Variable Elimination using Minimum Factor Size Heuristic

--- VariableElimination_Python/run.py
+++ VariableElimination_Python/run.py
@@ -7,7 +7,6 @@ Code to read in Bayesian Networks has been provided. We assume you have installe
 """
 from read_bayesnet import BayesNet
 from variable_elim import VariableElimination
-import os
 
 if __name__ == '__main__':
     # The class BayesNet represents a Bayesian network from a .bif file in several variables
@@ -25,19 +24,54 @@ if __name__ == '__main__':
 
     # Make your variable elimination code in the seperate file: 'variable_elim'. 
     # You use this file as follows:
-    #ve = VariableElimination(net)
+    ve = VariableElimination(net)
 
     # Set the node to be queried as follows:
-    #query = 'Alarm'
+    query = 'Alarm'
 
     # The evidence is represented in the following way (can also be empty when there is no evidence): 
-    #evidence = {'Burglary': 'True'}
+    evidence = {'Burglary': 'True'}
+
+    # Remove variables opposite of those observed (observed 'Burglary': True, so we remove 'Burglary': False)
+    cpts = [df for df in net.probabilities.values()]
+    updated_cpts = []
+    for df in cpts:
+        for key, value in evidence.items():
+            if key in df.columns:
+                df = df[df[key]==value]
+                updated_cpts.append(df)
+            else:
+                updated_cpts.append(df)
+
+    #Retrieve product formula:
+    product_formula = [list(df.columns[df.columns != 'prob']) for df in updated_cpts]
 
     # Determine your elimination ordering before you call the run function. The elimination ordering   
     # is either specified by a list or a heuristic function that determines the elimination ordering
     # given the network. Experimentation with different heuristics will earn bonus points. The elimination
     # ordering can for example be set as follows:
-    #elim_order = net.nodes
+    def minimum_factor_size(factor, evidence=set()):
+        elim_order = []
+        factor_copy = factor.copy()
+        variables = {var for factor in factor_copy for var in factor} - set(evidence.keys()) #All variables - evidence variable(s)
 
+        while variables:
+            sizes = {}
+            for var in variables:
+                involved = [set(factor) for factor in factor_copy if var in factor]
+                union_involved = set().union(*involved) #Concatenate all variables in involved into one set, removing duplicates and returning 1 single set.
+                sizes[var] = 2**len(union_involved) #2^k for boolean operations. 
+
+            best_var = min(sizes, key=sizes.get)
+            elim_order.append(best_var)
+            variables.remove(best_var)
+
+        return elim_order
+
+    print(minimum_factor_size(product_formula, evidence))
     # Call the variable elimination function for the queried node given the evidence and the elimination ordering as follows:   
-    #ve.run(query, evidence, elim_order)
+    result = ve.run(query, evidence, minimum_factor_size(product_formula, evidence))
+    print("-"*200)
+    print(f"CPT of variable {query}"); 
+    if evidence: print(f'given evidence {evidence}:')
+    print(result)
--- VariableElimination_Python/variable_elim.py
+++ VariableElimination_Python/variable_elim.py
@@ -15,7 +15,7 @@ class VariableElimination():
         """
         self.network = network
 
-    def run(self, query, observed, elim_order):
+    def run(self, query, observed_cpts, elim_order, factor_product_function):
         """
         Use the variable elimination algorithm to find out the probability
         distribution of the query variable given the observed variables
@@ -31,3 +31,34 @@ class VariableElimination():
                 for the query variable
 
         """
+
+        #Compute variable elimination:
+        for element in elim_order:
+            if element == query:
+                continue
+            factors_with_element = [df for df in observed_cpts if element in df.columns]
+            factors_without_element = [df for df in observed_cpts if element not in df.columns]
+
+            if not factors_with_element:
+                continue 
+
+            combined = factors_with_element[0]
+
+            for df in factors_with_element[1::]:
+                common = [column for column in combined.columns if column in df.columns and column !='prob']
+                combined = (combined.merge(df, on=common).assign(prob=lambda x: x.prob_x*x.prob_y).drop(columns=["prob_x", "prob_y"]))
+
+            keep = [column for column in combined.columns if column not in {element, 'prob'}] #Keep columns that do not consist of target column (following from elim_order)
+            new_factor = combined.groupby(keep, as_index=False)['prob'].sum()
+            
+            observed_cpts = factors_without_element + [new_factor]
+            factor_product_function = [[var for var in vars_ if var != element] for vars_ in factor_product_function]
+
+        #Sum out remaining vectors containing query variable
+        cpt_query = combined.groupby(query, as_index=False)['prob'].sum()
+        
+        #Marginalize
+        sum_rows = cpt_query['prob'].sum()
+        marginalized_cpt_query = cpt_query.copy()
+        marginalized_cpt_query['prob'] /= sum_rows
+        return marginalized_cpt_query
